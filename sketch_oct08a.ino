2d135dc6fc818969ea7bc0ade26e111774802c41
Georch9 <ie697761@iteso.mx>
2018-10-21 17:57:51 -0500

    thing speak with sensor and polution measurements

--- sketch_oct08a.ino
+++ sketch_oct08a.ino
@@ -1,18 +1,34 @@
-// Esta es la librería para utilizar las funciones de red del ESP8266
 #include <ESP8266WiFi.h> 
-
+#define MEDICIONES 6
+/******************************************          Wifi                        *************************************/
 const char* ssid = "iot"; // Rellena con el nombre de tu red WiFi
 const char* password = "12345678"; // Rellena con la contraseña de tu red WiFi
 
-//const char* host = "api.thingspeak.com";
-const char* host = "mfufif7r84.execute-api.us-west-2.amazonaws.com";
-//const char* apiKey = "/update?api_key=KPIV8WQLT57PSQ0C&field1="; 
-const char* apiKey = "/Airsense/01/NH3/";
+/******************************************          Get Urls                    *************************************/
+const char* host = "api.thingspeak.com";
+//const char* host = "mfufif7r84.execute-api.us-west-2.amazonaws.com";
+const char* apiKey = "/update?api_key=BPHU8TA1BBFR0N8Q&field"; 
+//const char* apiKey = "/Airsense/01/NH3/";
 //https://mfufif7r84.execute-api.us-west-2.amazonaws.com/Airsense/userID/type/value
 
-//Funciones
+/******************************************          Variables globales          *************************************/
+int adc_MQ;
+float voltaje;
+float Rs ;
+String url;
+int n;
+/******************************************          Funciones                   *************************************/
 double getCO2();
+double analog_alcohol(float Rs);
+double analog_amoniaco(float Rs);
+double analog_metano(float Rs);
+double analog_propano(float Rs);
+double analog_monoxDeCarbono(float Rs);
+double analog_hidrogeno(float Rs);
+double analog_dioxidoDeCarbono(float Rs);
+double (*analog_lectur[6])(float) = {analog_amoniaco, analog_metano, analog_propano, analog_monoxDeCarbono, analog_hidrogeno, analog_dioxidoDeCarbono};
 
+/******************************************          Configuracion inicial      *************************************/
 void setup() 
 {
   Serial.begin(115200);
@@ -41,8 +57,9 @@ void setup()
   Serial.println("WiFi connected"); 
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP()); // Mostramos la IP
-}
 
+}
+/******************************************          Main                    *************************************/
 void loop() 
 {
 
@@ -52,61 +69,64 @@ void loop()
   // Creamos el cliente
   WiFiClient client;
   const int httpPort = 80; // Puerto HTTP
-   // Creamos la URL para la petición
-  String url;
+
  
   while(1)
   {
 
+    adc_MQ  = analogRead(A0); //Leemos la salida analógica del MQ
+    voltaje = adc_MQ * (5.0 / 1023.0); //Convertimos la lectura en un valor de voltaje
+    Rs = 1000 * ((5 - voltaje)/voltaje); //Calculamos Rs con un RL de 1k
 
-    if (!client.connect(host, httpPort)) 
+
+    
+    for(n = 1; n <= MEDICIONES; n++)
     {
-      // ¿hay algún error al conectar?
-      Serial.println("Ha fallado la conexión");
-      return;
-    }
-    Serial.println("Conexión establecida");
-    url = "";
-    url += apiKey + String(getCO2());
+      if (!client.connect(host, httpPort)) 
+      {
+        // ¿hay algún error al conectar?
+        Serial.println("Ha fallado la conexión");
+        return;
+      }
+      Serial.println("Conexión establecida");
 
-    Serial.print("URL de la petición: http://");
-    Serial.print(host);
-    Serial.println(url);
-    // Enviamos la petición
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-         "Host: " + host + "\r\n" + 
-         "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) 
-    {
-      if (millis() - timeout > 50000) 
-        {
-          Serial.println(">>> Superado el tiempo de espera !");
-          client.stop();
-          return;
-        }
+      url = "";
+      url += apiKey;
+      url += String(n) + "=";
+      url += String((*analog_lectur[n -1])(Rs));
+      Serial.print("URL de la petición: http://");
+      Serial.print(host);
+      Serial.println(url);
+      // Enviamos la petición
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+           "Host: " + host + "\r\n" + 
+           "Connection: close\r\n\r\n");
+      unsigned long timeout = millis();
+      while (client.available() == 0) 
+      {
+        if (millis() - timeout > 50000) 
+          {
+            Serial.println(">>> Superado el tiempo de espera !");
+            client.stop();
+            return;
+          }
+      }
+    // Consutar la memoria libre
+    // Quedan un poco más de 40 kB
+      Serial.printf("\nMemoria libre en el ESP8266: %d Bytes\n\n",ESP.getFreeHeap());
+   
+    // Leemos la respuesta y la enviamos al monitor serie
+      while(client.available())
+      {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+      }
+   
+      Serial.println();
+      Serial.println("Cerrando la conexión");
+      delay(10000);
     }
-
-  // Consutar la memoria libre
-  // Quedan un poco más de 40 kB
-  Serial.printf("\nMemoria libre en el ESP8266: %d Bytes\n\n",ESP.getFreeHeap());
- 
-  // Leemos la respuesta y la enviamos al monitor serie
-  while(client.available())
-  {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
- 
-  Serial.println();
-  Serial.println("Cerrando la conexión");
-  delay(50000);
-}
-  while(1){
-    delay(0); // Siempre que hay un bucle que pueda durar mucho tiempo
-              // hay que llamar a la función delay() para atender a los 
-              // procesos de la conexión WiFi. Si no se hace el ESP8266
-              // generará un error y se reiniciará a los pocos segundos
+    delay(50000);
   }
 }
 
@@ -128,5 +148,48 @@ double getCO2()
   Serial.print(alcohol);
   Serial.println("mg/L");  
 }
+
+double analog_alcohol(float Rs)
+{
+  double alcohol = 1.108*pow(Rs/5463, -1.41); //Calculamos la concentración del alcohol
+  return alcohol; 
+}
+
+double analog_amoniaco(float Rs)
+{
+  double amoniaco = 161.7*pow(Rs/5463, -2.26); //Calculamos la concentración del amoniaco
+  return amoniaco; 
+}
+
+double analog_metano(float Rs)
+{
+  double metano = 6922*pow(Rs/5463, -1.91); //Calculamos la concentración del metano
+  return metano;
+}
+
+double analog_propano(float Rs)
+{
+  double propano = 2738*pow(Rs/5463, -1.81); //Calculamos la concentración del propano
+  return propano;
+}
+
+double analog_monoxDeCarbono(float Rs)
+{
+  double monoxDeCarbono = 233.9*pow(Rs/5463, -1.40);
+  return monoxDeCarbono;
+}
+
+double analog_hidrogeno(float Rs)
+{
+  double hidrogeno = 1803*pow(Rs/5463, -0.66);
+  return hidrogeno;
+}
+
+double analog_dioxidoDeCarbono(float Rs)
+{
+  double dioxidoDeCarbono = 245*pow(Rs/5463, 2.26); 
+  return dioxidoDeCarbono;
+}
+
 
 
